2c1888d1cb968544368d8a71c4e8adcacafa0217
Leon Spreckels <110732125+leon20spr@users.noreply.github.com>
2022-11-09 21:21:30 +0100

    added helpers.index.ts / client

---
+++ src/mysql-client/client-helpers/helpers.index.ts
@@ -0,0 +1,11 @@
+import MySQLClientHelpersHandlerConsoleIndex from "./helpers-handlers/handler.console";
+
+class MySQLClientHelpersIndex {
+    public console: MySQLClientHelpersHandlerConsoleIndex;
+
+    public constructor() {
+        this.console = new MySQLClientHelpersHandlerConsoleIndex();
+    }
+}
+
+export default MySQLClientHelpersIndex;
