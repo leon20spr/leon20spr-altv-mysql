80d878c43f35fe7482f2b7a936c62d575e84a2eb
Leon Spreckels <110732125+leon20spr@users.noreply.github.com>
2022-11-09 21:21:05 +0100

    edited handler.console.ts / server
    [+] error
    [+] log
    [+] warning

--- src/mysql-server/server-helpers/helpers-handlers/handler.console.ts
+++ src/mysql-server/server-helpers/helpers-handlers/handler.console.ts
@@ -1,3 +1,46 @@
-class MySQLServerHelpersHandlerConsoleIndex {}
+import * as alt from "alt-server";
+
+import mysql from "../../server.index";
+
+class MySQLServerHelpersHandlerConsoleIndex {
+    /**
+     * To format errors 
+     * @param {string} location 
+     * @param {string} text 
+     */
+    public error(location: string, text: string): void {
+        try {
+            alt.log(`~lr~[ERROR] ~w~[${location}] ~w~${text}`);
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    /**
+     * To format logs
+     * @param {string} location 
+     * @param {string} text 
+     */
+    public log(location: string, text: string): void {
+        try {
+            alt.log(`~lg~[LOG] ~w~[${location}] ~w~${text}`);
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    /**
+     * To format warnings
+     * @param {string} location 
+     * @param {string} text 
+     */
+    public warning(location: string, text: string): void {
+        try {
+            alt.log(`~ly~[WARNING] ~w~[${location}] ~w~${text}`);
+        } catch (error: any) {
+            throw error;
+        }
+    }
+}
 
 export default MySQLServerHelpersHandlerConsoleIndex;
